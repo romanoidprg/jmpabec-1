78707e4295a5991ea7d007b6d388cf725756d25a
raman_padstsenny <raman_padstsenny@epam.com>
2023-04-10 10:49:33 +0300

    add func

--- jmp-service-api/src/main/java/com/epam/jmpabec/service/api/Service.java
+++ jmp-service-api/src/main/java/com/epam/jmpabec/service/api/Service.java
@@ -4,6 +4,9 @@ import com.epam.jmpabec.dto.BankCard;
 import com.epam.jmpabec.dto.Subscription;
 import com.epam.jmpabec.dto.User;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,4 +14,10 @@ public interface Service {
     void subscribe(BankCard card);
     Optional<Subscription> getSubscriptionByBankCardNumber(String number);
     List<User> getAllUsers();
+    default double getAverageUsersAge() {
+        return getAllUsers().stream().mapToLong(u ->  ChronoUnit.YEARS.between(u.getBirthday(), LocalDate.now())).average().orElse(0);
+    };
+    static boolean isPayableUser(User user) {
+        return ChronoUnit.YEARS.between(user.getBirthday(), LocalDate.now()) > 18;
+    };
 }
--- jmp.application/src/main/java/com/epam/jmpabec/Main.java
+++ jmp.application/src/main/java/com/epam/jmpabec/Main.java
@@ -5,17 +5,19 @@ import com.epam.jmpabec.dto.BankCard;
 import com.epam.jmpabec.dto.BankCardType;
 import com.epam.jmpabec.dto.Subscription;
 import com.epam.jmpabec.dto.User;
+import com.epam.jmpabec.service.api.Service;
 import com.epam.jmpabec.service.impl.ServiceImpl;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         final var dart = new User("Dart", "Veider", LocalDate.of(1920, 4, 1));
-        final var sasha = new User("Sasha", "Pushkin", LocalDate.of(1799, 6, 6));
-        final var chewbacca = new User("Chew", "Vuki", LocalDate.of(3545, 1, 1));
+        final var sasha = new User("Sasha", "Pushkin", LocalDate.of(2020, 6, 6));
+        final var chewbacca = new User("Chew", "Vuki", LocalDate.of(233, 1, 1));
         var users = List.of(dart, sasha, chewbacca);
         var cards = new ArrayList<BankCard>();
 
@@ -29,7 +31,7 @@ public class Main {
 
         cards.forEach(c -> {
             try {
-                Thread.sleep(2000);
+                Thread.sleep(500);
                 service.subscribe(c);
                 System.out.println("Card with number " + c.getNumber() + " subscribed.");
             } catch (InterruptedException e) {
@@ -37,8 +39,17 @@ public class Main {
             }
         });
 
-        service.getAllUsers().forEach(u -> System.out.println(u.getName() + u.getSurname() + u.getBirthday()));
-        cards.forEach(c -> System.out.println(c.getNumber()+c.getUser()));
+        System.out.println("All users:");
+        service.getAllUsers().forEach(System.out::println);
+
+        System.out.println("Playable users:");
+        service.getAllUsers().stream().filter(Service::isPayableUser).collect(Collectors.toUnmodifiableList()).forEach(System.out::println);
+        System.out.println();
+
+        System.out.println("Average users age is: " + service.getAverageUsersAge());
+        System.out.println();
+
+        cards.forEach(c -> System.out.println(c.getNumber() + " " + c.getUser()));
         service.getSubscriptionByBankCardNumber(cards.get(0).getNumber()).ifPresentOrElse(Main::prSscr, Main::prNoSscr);
         service.getSubscriptionByBankCardNumber(cards.get(2).getNumber()).ifPresentOrElse(Main::prSscr, Main::prNoSscr);
         service.getSubscriptionByBankCardNumber(cards.get(5).getNumber()).ifPresentOrElse(Main::prSscr, Main::prNoSscr);
